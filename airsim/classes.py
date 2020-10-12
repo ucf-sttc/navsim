62f89401e81d123a5224de9110dc95a4b90ba886
armando-fandango <armando@neurasights.com@users.noreply.github.com>
2020-10-11 22:17:42 -0400

    memory class generalized for obs mode 0,1,2
    Memory class generalized and tested for obs_mode 0,1,2

--- airsim/classes.py
+++ airsim/classes.py
@@ -1,3 +1,5 @@
+import random
+
 from mlagents_envs.environment import UnityEnvironment
 from gym_unity.envs import UnityToGymWrapper
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
@@ -5,15 +7,18 @@ from mlagents_envs.side_channel.environment_parameters_channel import Environmen
 
 import torch
 import torch.nn.functional as F
+from numpy.core._multiarray_umath import ndarray
 from torch import nn
 import numpy as np
 from tqdm import tqdm
 
 from copy import deepcopy
 
+from typing import Union, Optional
+
 
 class AirSimEnv:
-    def __init__(self, filename=None):
+    def __init__(self, filename: Optional[str] = None, observation_mode: int = 0):
         engine_side_channel = EngineConfigurationChannel()
         environment_side_channel = EnvironmentParametersChannel()
         uenv = UnityEnvironment(file_name=filename,
@@ -26,12 +31,14 @@ class AirSimEnv:
         environment_side_channel.set_float_parameter("rewardForFallingOffMap", -1)
         environment_side_channel.set_float_parameter("rewardForEachStep", -.0001)
         environment_side_channel.set_float_parameter("segmentationMode", 1)
+        environment_side_channel.set_float_parameter("observationMode", observation_mode)
 
         genv = UnityToGymWrapper(uenv, False, False,
                                  True)  # (Environment, uint8_visual, flatten_branched, allow_multiple_obs)
         genv.action_space.np_random.seed(123)
         self.uenv = uenv
         self.genv = genv
+        self.observation_mode = observation_mode
         self.n_x = 7
         self.n_y = 2
 
@@ -39,38 +46,57 @@ class AirSimEnv:
         self.uenv.close()
 
     def info(self):
-        genv = self.genv
-        if genv.spec:
-            print(genv.spec.id)
-        print('Action Space:', genv.action_space)
-        print('Action sample:', genv.action_space.sample())
-        print('Action Space Shape:', genv.action_space.shape)
-        print('Action Space Low:', genv.action_space.low)
-        print('Action Space High:', genv.action_space.high)
-        print('Gym Observation Space:', genv.observation_space)
-        print('Gym Observation Space Shape:', genv.observation_space.shape)
+        print("Env Info")
+        print('-----------')
+        if self.genv.spec:
+            print(self.genv.spec.id)
+        print('Action Space:', self.action_space)
+        print('Action sample:', self.action_space.sample())
+        print('Action Space Shape:', self.action_space.shape)
+        print('Action Space Low:', self.action_space.low)
+        print('Action Space High:', self.action_space.high)
+        print('Observation Mode:', self.observation_mode)
+        print('Gym Observation Space:', self.genv.observation_space)
+        print('Gym Observation Space Shape:', self.genv.observation_space.shape)
         print('Self Observation Space:', self.observation_space)
         print('Self Observation Space Shape:', self.observation_space.shape)
-        print('Reward Range:', genv.reward_range)
-        print('Metadata:', genv.metadata)
-        print('Initial State:', genv.reset())
-        print('First Step State:', genv.step(genv.action_space.sample()))
+        print('Reward Range:', self.genv.reward_range)
+        print('Metadata:', self.genv.metadata)
+        print('Initial State:', self.reset())
+        print('First Step State:', self.step(self.action_space.sample()))
 
     @property
     def action_space(self):
         return self.genv.action_space
 
     @property
+    def action_space_shape(self):
+        return self.action_space.shape
+
+    @property
     def observation_space(self):
         """
         We can control here what observation space to return
+        Observation Space is always returned as a tuple
         :return:
         """
-        return self.genv.observation_space[0]
+        return self.genv.observation_space
+
+    @property
+    def observation_space_shapes(self):
+        """
+        Returns the dimensions of the observation space
+        :return:
+        """
+        return [obs.shape for obs in self.observation_space]
 
     @property
     def gym_env(self):
         return self.genv
+
+    @property
+    def unity_env(self):
+        return self.uenv
 
     def step(self, a):
         return self.genv.step(a)
@@ -82,30 +108,127 @@ class AirSimEnv:
         return self.genv.seed(seed)
 
 
-class Memory():
+class Memory:
     """
-    Stores and returns list of numpy arrays
+    Stores and returns list of numpy arrays for s a r s_ d
+
+    assumes you are storing s a r s_ d : state action reward next_state done
+    # state / next_state : tuple of n-dim numpy arrays
+    # a : n-dim numpy array of floats or ints
+    # r : 1-d numpy array of floats #TODO: rewards could also be n-dim
+
+    # TODO: see if sampling can be made better with np.random
+    # TODO: if we made this tensor based instead of np, would it make it better ?
     """
+    @classmethod
+    def sample_info(self,s,a,r,s_,d):
+        print("Sample Info")
+        print('-----------')
+        print("Shapes:")
+        print('s:',[a.shape for a in s])
+        print('a:',a.shape)
+        print('r:',r.shape)
+        print('s_:',[a.shape for a in s_])
+        print('d:',d.shape)
 
-    def __init__(self, capacity, seed=None, mem=[]):
-        self.cap = capacity
-        self.mem = mem
-        self.ptr = 0
-        self.seed = seed
-        seed or random.seed(seed)
+    def __init__(self, capacity: int,
+                 state_shapes: Union[list, tuple, int],
+                 action_shape: Union[list, tuple, int],
+                 seed: Optional[float] = None):
+        """
 
-    def append(self, item):
-        if len(self.mem) < self.cap:
-            self.mem.append(None)
-        self.mem[self.ptr] = item
-        self.ptr = (self.ptr + 1) % self.cap
+        :param capacity: total capacity of the memory
+        :param state_shapes: a tuple of state dimensions
+        :param action_shape: int representing number of actions
+        :param seed: seed to provide to numpy #TODO How to get separate seed just for the memory object ?
+        """
+        self.capacity = capacity
+        self.ptr: int = 0
+        self.size: int = 0
+
+        self.seed: Optional[float] = seed
+        #from numpy.random import MT19937, RandomState, Generator
+        #mt = MT19937(seed)
+        #rs = RandomState(mt)
+        #rg = Generator(mt)
+        self.rng = np.random.default_rng(seed)
+
+        if not isinstance(state_shapes, (list, tuple)): # state_dims is an int
+            state_shapes = [[state_shapes]]  # make state_dims a list of sequences
+        elif not isinstance(state_shapes[0], (list, tuple)):  # state_dims first member is an int
+            state_shapes = [state_shapes]  # make state_dims a list of sequences
+        # else state_dims is a list of sequences, i.e. state_shape, state_shape, state_shape
+
+        s_len = len(state_shapes)
+        self.s = [None] * s_len
+        self.s_ = [None] * s_len
+
+        for i in range(s_len):
+            state_shapes[i] = [capacity] + list(state_shapes[i])
+            self.s[i] = np.full(state_shapes[i], 0.0)
+            self.s_[i] = np.full(state_shapes[i], 0.0)
+
+        action_shape = [capacity] + list(action_shape)
+        self.a = np.full(action_shape, 0.0)
+
+        self.r = np.full((capacity, 1), 0.0)
+        self.d = np.full((capacity, 1), False)
+
+    def append(self, s:Union[list, tuple, int, float],
+               a: Union[list, tuple, int, float],
+               r: float,
+               s_: Union[list, tuple, int, float],
+               d: int):
+        if not isinstance(s, (list, tuple)): # state is an int
+            s = [[s]]  # make state a list of sequences
+            s_ = [[s_]]
+        elif not isinstance(s[0], (list, tuple, np.ndarray)):  # state does not have multiple seq
+            s = [s]
+            s_ = [s_]
+        for i in range(len(s)):
+            self.s[i][self.ptr] = s[i]
+            self.s_[i][self.ptr] = s_[i]
+
+        self.a[self.ptr] = a
+        self.r[self.ptr] = r
+        self.d[self.ptr] = d
+
+        self.ptr = (self.ptr + 1) % self.capacity
+        self.size = min(self.size + 1, self.capacity)
 
     def sample(self, size):
-        # TODO: see if it can be made better with numpy
-        if (size == 0) or (size >= len(self.mem)):
-            return self.mem  # just return all
+        if (size == 0) or (size >= self.size):
+            #idx = list(range(self.size))   # just return all
+            return (
+                self.s,
+                self.a,
+                self.r,
+                self.s,
+                self.d
+            )
         else:
-            return random.sample(self.mem, size)
+            idx = self.rng.integers(low=0, high=self.size, size=size)
+            s_len = len(self.s)
+            return (
+                [self.s[i][idx] for i in range(s_len)],
+                self.a[idx],
+                self.r[idx],
+                [self.s_[i][idx] for i in range(s_len)],
+                self.d[idx]
+            )
+
+    def info(self):
+        print("Memory Info")
+        print('-----------')
+        print('capacity:',self.capacity)
+        print('size:',self.size)
+        print('seed:',self.seed)
+        print("Shapes:")
+        print('s:',[a.shape for a in self.s])
+        print('a:',self.a.shape)
+        print('r:',self.r.shape)
+        print('s_:',[a.shape for a in self.s_])
+        print('d:',self.d.shape)
 
     def __len__(self):
         return len(self.mem)
@@ -114,10 +237,10 @@ class Memory():
         return str(self.mem)
 
     def __str__(self):
-        return f'capacity={self.cap} \nlength={len(self.mem)} \ncurrent pointer={self.ptr} \nmemory contents:\n{self.mem}'
+        return f'capacity={self.capacity} \nlength={len(self.mem)} \ncurrent pointer={self.ptr} \nmemory contents:\n{self.mem}'
 
     def __getitem__(self, sliced):
-        return Memory(capacity=self.cap, seed=self.seed, mem=self.mem[sliced])
+        return Memory(capacity=self.capacity, seed=self.seed, mem=self.mem[sliced])
 
     def pretty_print(self, n_rows=0):
         """
@@ -168,12 +291,21 @@ class ReplayBuffer(object):
 
     def sample(self, batch_size):
         ind = np.random.randint(0, self.size, size=batch_size)
+        """
         return (
             torch.FloatTensor(self.state[ind]).to(self.device),
             torch.FloatTensor(self.action[ind]).to(self.device),
             torch.FloatTensor(self.next_state[ind]).to(self.device),
             torch.FloatTensor(self.reward[ind]).to(self.device),
             torch.FloatTensor(self.not_done[ind]).to(self.device)
+        )
+        """
+        return (
+            self.state[ind],
+            self.action[ind],
+            self.next_state[ind],
+            self.reward[ind],
+            self.not_done[ind]
         )
 
 
@@ -236,48 +368,75 @@ class AirSimNN(object):
         print(self.model)
 
 
-class Actor(torch.nn.Module):
+class ActorVector(torch.nn.Module):
 
     def __init__(self, state_dimension, action_dimension, max_action):
-        super(Actor, self).__init__()
+        super(ActorVector, self).__init__()
         self.l1 = torch.nn.Linear(state_dimension, 400)
         self.l2 = torch.nn.Linear(400, 300)
         self.l3 = torch.nn.Linear(300, action_dimension)
         self.max_action = max_action
 
     def forward(self, state):
-        #state = state[0]
+        """
+
+        :param state: state is a torch tensor
+        :return:
+        """
+        # state = state[0]
         a = F.relu(self.l1(state))
         a = F.relu(self.l2(a))
-        return self.max_action * torch.tanh(self.l3(a))
+        a = torch.tanh(self.l3(a))
+        return self.max_action * a
 
 
-class Critic(torch.nn.Module):
+class CriticVector(torch.nn.Module):
 
     def __init__(self, state_dimension, action_dimension):
-        super(Critic, self).__init__()
+        super(CriticVector, self).__init__()
         self.l1 = torch.nn.Linear(state_dimension + action_dimension, 400)
         self.l2 = torch.nn.Linear(400, 300)
         self.l3 = torch.nn.Linear(300, 1)
 
     def forward(self, state, action):
-        #state = state[0]
+        # state = state[0]
         q = F.relu(self.l1(torch.cat([state, action], 1)))
         q = F.relu(self.l2(q))
-        return self.l3(q)
+        q = self.l3(q)
+        return q
 
 
 class DDPGAgent(object):
 
-    def __init__(self, state_dim, action_dim, max_action, device, discount=0.99, tau=0.005):
+    def __init__(self, observation_mode, observation_space, action_space, device, discount=0.99, tau=0.005):
+        self.observation_mode = observation_mode
+        self.observation_space = observation_space
+        self.action_space = action_space
         self.device = device
         self.discount = discount
         self.tau = tau
-        self.state_dim = state_dim
-        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        # self.state_dim = state_dim
+
+        self.action_dimension = action_space.shape[0]
+        self.max_action = float(action_space.high[0])
+
+        if observation_mode == 0:
+            self.vector_state_dimension = observation_space[0].shape[0]
+        elif observation_mode == 1:
+            self.vector_state_dimension = None
+        elif observation_mode == 2:
+            self.vector_state_dimension = observation_space[3].shape[0]
+
+        if (observation_mode == 0) or (observation_mode == 2):
+            self.actor = ActorVector(self.vector_state_dim, self.action_dim, self.max_action).to(device)
+            self.critic = CriticVector(self.vector_state_dim, self.action_dim).to(device)
+        else:  # TODO: Implement VisualActor here
+            self.actor = None
+            self.critic = None
+
         self.actor_target = deepcopy(self.actor)
         self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
-        self.critic = Critic(state_dim, action_dim).to(device)
+
         self.critic_target = deepcopy(self.critic)
         self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
 
@@ -298,7 +457,8 @@ class DDPGAgent(object):
 
     def save_actor(self, filename):
         dummy_input = torch.randn(self.state_dim).to(self.device)
-        torch.onnx.export(self.actor,dummy_input,filename,export_params=True,opset_version=9,do_constant_folding=True,
+        torch.onnx.export(self.actor, dummy_input, filename, export_params=True, opset_version=9,
+                          do_constant_folding=True,
                           input_names=['state'], output_names=['action'])
 
     def load_checkpoint(self, filename):
@@ -364,22 +524,36 @@ def evaluate_policy(policy, env, seed, eval_episodes=10, render=False):
 
 class Trainer:
 
-    def __init__(self, conf, env, enable_logging=False):
+    def __init__(self, config, env, enable_logging=False):
+        """
+
+        :param config: A DictObj containing dictionary and object interface
+        :param env:
+        :param enable_logging:
+        """
         self.enable_logging = enable_logging
-        self.config = conf
+        self.config = config
         self.env = env
+
         self.apply_seed()
-        self.state_dimension = self.env.observation_space.shape[0]
-        self.action_dimension = self.env.action_space.shape[0]
-        self.max_action = float(self.env.action_space.high[0])
+
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
         self.agent = DDPGAgent(
-            state_dim=self.state_dimension, action_dim=self.action_dimension,
-            max_action=self.max_action, device=self.device,
+            observation_mode=self.env.observation_mode,
+            observation_space=self.env.observation_space,
+            action_space=self.env.action_space,
+            # state_dim=self.vector_state_dimension,
+            # action_dim=self.action_dimension,
+            # max_action=self.max_action, device=self.device,
             discount=self.config['discount'], tau=self.config['tau']
         )
+
+        # TODO: Give file and folder names better names based on the experiment_id
+
         self.save_file_name = f"DDPG_{self.config['env_name']}_{self.config['seed']}"
-        self.memory = ReplayBuffer(self.state_dimension, self.action_dimension)
+        self.memory = Memory(
+            self.config.memory_capacity)  # ReplayBuffer(self.vector_state_dimension, self.action_dimension)
         if self.enable_logging:
             from torch.utils.tensorboard import SummaryWriter
             self.writer = SummaryWriter('./logs/' + self.config['env_name'] + '/')
@@ -392,9 +566,10 @@ class Trainer:
         self.env.seed(self.config['seed'])
         torch.manual_seed(self.config['seed'])
         np.random.seed(self.config['seed'])
+        self.memory.apply_seed(self.config['seed'])
 
     def train(self):  # at three places state has been selected as 0th element
-        state = self.env.reset()[0]
+        state = self.env.reset()
         done = False
         episode_reward = 0
         episode_timesteps = 0
@@ -407,7 +582,7 @@ class Trainer:
                 action = self.env.action_space.sample()
             else:
                 action = (
-                        self.agent.select_action(np.array(state)) + np.random.normal(
+                        self.agent.select_action(state) + np.random.normal(
                     0, self.max_action * self.config['expl_noise'],
                     size=self.action_dimension
                 )
